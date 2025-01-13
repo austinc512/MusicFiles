b337d02e532a6d6de19c8a79edd0887b28eb1972
austinc512 <cello.austin1994@gmail.com>
2025-01-13 00:03:02 -0600

    moved logic into startup extensions

--- src/MusicFiles.WebAPI/Program.cs
+++ src/MusicFiles.WebAPI/Program.cs
@@ -1,78 +1,9 @@
-using Amazon.Runtime;
-using Amazon.Runtime.CredentialManagement;
-using Amazon.S3;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-using MusicFiles.Core.Domain.IdentityEntities;
-using MusicFiles.Core.RepositoryContracts;
-using MusicFiles.Core.ServiceContracts;
-using MusicFiles.Core.Services;
-using MusicFiles.Infrastructure.Data;
-using MusicFiles.Infrastructure.Services;
-using MusicFiles.Infrastructure.Repositories;
+using MusicFiles.WebAPI.StartupExtensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var credentials = LoadAwsCredentials(
-    builder.Configuration
-        .GetRequiredSection("AWS")
-        .GetValue<string>("ProfileName") 
-    ?? throw new InvalidOperationException("Profile name must be configured.")
-);
-
-// File storage
-builder.Services.AddSingleton<IAmazonS3>(_ => new AmazonS3Client(credentials));
-// S3Service (Infra Service; Singleton) because it depends on IAmazonS3 (Singleton)
-builder.Services.AddSingleton<IS3Service>(sp =>
-    new S3Service(
-        sp.GetRequiredService<IAmazonS3>(),
-        builder.Configuration.GetRequiredSection("AWS").GetValue<string>("BucketName") 
-        ?? throw new InvalidOperationException("Bucket name must be configured.")
-    ));
-// FileUploadService (Core Service; Scoped) because it may perform request-specific logic in the future
-builder.Services.AddScoped<IFileUploadService, FileUploadService>();
-
-// check environment
-var environment = builder.Environment;
-
-// Data persistence
-// DbContext's ServiceLifetime is Scoped by default
-// DbContext (Infra) => Repository (Infra) => DataService (Core)
-// Making all of these scoped guarantees thread safety and avoids lifecycle conflicts
-builder.Services.AddDbContext<MusicFilesDbContext>(options =>
-    {
-        options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
-        if (environment.IsDevelopment())
-        {
-            // useful for troubleshooting the postgres connection.
-            options.EnableSensitiveDataLogging()
-                .EnableDetailedErrors();
-        }
-    }
-    );
-
-builder.Services.AddIdentity<ApplicationUser, ApplicationRole>(options =>
-    {
-        options.User.RequireUniqueEmail = true;
-        //  The default ASP.NET Identity database migration already applies a unique constraint on the UserName column.
-    })
-    .AddEntityFrameworkStores<MusicFilesDbContext>()
-    .AddDefaultTokenProviders()
-    .AddUserStore<UserStore<ApplicationUser, ApplicationRole, MusicFilesDbContext, Guid>>()
-    .AddRoleStore<RoleStore<ApplicationRole, MusicFilesDbContext, Guid>>();
-
-builder.Services.AddScoped<IMusicFilesRepository, MusicFilesRepository>();
-builder.Services.AddScoped<IMusicDataService, MusicDataService>();
-
-builder.Services.AddAuthorization(options =>
-{
-// Endpoints will require authentication unless overridden with [AllowAnonymous]
-    options.FallbackPolicy = new AuthorizationPolicyBuilder()
-        .RequireAuthenticatedUser()
-        .Build();
-});
+// startup extensions:
+builder.Services.ConfigureServices(builder.Configuration, builder.Environment);
 
 
 // Register services for Controllers
@@ -89,18 +20,3 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
-
-// Helper method to load AWS credentials
-static AWSCredentials LoadAwsCredentials(string profileName)
-{
-    var credentialsFile = new SharedCredentialsFile();
-    if (credentialsFile.TryGetProfile(profileName, out var profile))
-    {
-        AWSCredentialsFactory.TryGetAWSCredentials(profile, credentialsFile, out var credentials);
-        return credentials;
-    }
-    else
-    {
-        throw new Exception($"Profile {profileName} not found in credentials file.");
-    }
-}
---
+++ src/MusicFiles.WebAPI/StartupExtensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,94 @@
+using Amazon.Runtime;
+using Amazon.Runtime.CredentialManagement;
+using Amazon.S3;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MusicFiles.Core.Domain.IdentityEntities;
+using MusicFiles.Core.RepositoryContracts;
+using MusicFiles.Core.ServiceContracts;
+using MusicFiles.Core.Services;
+using MusicFiles.Infrastructure.Data;
+using MusicFiles.Infrastructure.Repositories;
+using MusicFiles.Infrastructure.Services;
+
+namespace MusicFiles.WebAPI.StartupExtensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection ConfigureServices(this IServiceCollection services, IConfiguration configuration, IWebHostEnvironment environment)
+    {
+        var credentials = LoadAwsCredentials(configuration
+                 .GetRequiredSection("AWS")
+                 .GetValue<string>("ProfileName") 
+                 ?? throw new InvalidOperationException("Profile name must be configured."));
+        
+        // File storage
+        services.AddSingleton<IAmazonS3>(_ => new AmazonS3Client(credentials));
+        // S3Service (Infra Service; Singleton) because it depends on IAmazonS3 (Singleton)
+        services.AddSingleton<IS3Service>(sp =>
+            new S3Service(
+                sp.GetRequiredService<IAmazonS3>(),
+                configuration.GetRequiredSection("AWS").GetValue<string>("BucketName") 
+                ?? throw new InvalidOperationException("Bucket name must be configured.")
+            ));
+        // FileUploadService (Core Service; Scoped) because it may perform request-specific logic in the future
+        services.AddScoped<IFileUploadService, FileUploadService>();
+        
+        // Data persistence: the entire chain of services is scoped to guarantee thread safety and avoid lifecycle conflicts
+        // DbContext (Infra) => Repository (Infra) => DataService (Core)
+        services.AddDbContext<MusicFilesDbContext>(options =>
+            {
+                options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"));
+                if (environment.IsDevelopment())
+                {
+                    // useful for troubleshooting the postgres connection.
+                    options.EnableSensitiveDataLogging()
+                        .EnableDetailedErrors();
+                }
+            }
+        );
+        
+        // Identity
+        services.AddIdentity<ApplicationUser, ApplicationRole>(options =>
+            {
+                options.User.RequireUniqueEmail = true;
+                //  The default ASP.NET Identity database migration already applies a unique constraint on the UserName column.
+            })
+            .AddEntityFrameworkStores<MusicFilesDbContext>()
+            .AddDefaultTokenProviders()
+            .AddUserStore<UserStore<ApplicationUser, ApplicationRole, MusicFilesDbContext, Guid>>()
+            .AddRoleStore<RoleStore<ApplicationRole, MusicFilesDbContext, Guid>>();
+
+        services.AddScoped<IMusicFilesRepository, MusicFilesRepository>();
+        services.AddScoped<IMusicDataService, MusicDataService>();
+
+        services.AddAuthorization(options =>
+        {
+            // Endpoints will require authentication unless overridden with [AllowAnonymous]
+            options.FallbackPolicy = new AuthorizationPolicyBuilder()
+                .RequireAuthenticatedUser()
+                .Build();
+        });
+        
+        
+        return services;
+    }
+    
+    // Helper method to load AWS credentials
+    static AWSCredentials LoadAwsCredentials(string profileName)
+    {
+        var credentialsFile = new SharedCredentialsFile();
+        if (credentialsFile.TryGetProfile(profileName, out var profile))
+        {
+            AWSCredentialsFactory.TryGetAWSCredentials(profile, credentialsFile, out var credentials);
+            return credentials;
+        }
+        else
+        {
+            throw new Exception($"Profile {profileName} not found in credentials file.");
+        }
+    }
+}
+
